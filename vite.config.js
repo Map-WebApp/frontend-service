0852e598fe8ba52230150819284269015680e919
Trần Hoài Phú <phubao789@gmail.com>
2025-06-14 15:21:38 +0700

    build: configure vite proxy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api/auth': 'http://localhost:3001',
+      '/api/users': 'http://localhost:8080',
+      '/locations': 'http://localhost:3003',
+      '/directions': 'http://localhost:3004',
+      '/socket.io': {
+        target: 'ws://localhost:3002',
+        ws: true,
+      },
+    }
   }
 });
